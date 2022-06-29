2552c4892ce8322c5800dba713ef557f89c4a31e
Vetrov Vadim <vetrovvd@yandex.ru>
2022-06-29 12:49:25 +0300

    mangalib parser can parse chapters

--- websites_modules/mangalib_me.py
+++ websites_modules/mangalib_me.py
@@ -1,7 +1,14 @@
+from posixpath import split
+import string
+from websites_modules.init_webdriver import init_webdriver
 import requests
 import re
 import json
-from init_webdriver import init_webdriver
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+from selenium.webdriver.common.by import By
+from selenium import webdriver
 
 
 def execute():
@@ -27,29 +34,132 @@ def download_by_url():
     # Ask user for manga url
     url = input("Please enter manga url:\n")
 
-    url = re.search('mangalib.me/.*', url)
+    print("Parsing url...")
+    url = urlparse(url)
 
-    if url:
-        url = url.string
-        if url.startswith("https://"):
+    if url.netloc == "mangalib.me" or url.netloc == "www.mangalib.me":
+        if url.scheme == "https":
             pass
-        elif url.startswith("http://"):
+        elif url.scheme == "http":
             print(
                 "Url passed but using http protocol. It will be automatically replaced by https")
-            url = url.replace("http://", "https://")
+            url = url._replace(scheme="https")
         else:
             print("Url passed but no protocol specified. Https will be used")
-            url = f"https://{url}"
+            url._replace(scheme="https")
     else:
         return print("Url is incorrect")
 
-    print("Intializing webdriver. Please wait. This may take a while")
+    url_path = [x for x in url.path.split('/') if x]
 
-    webdriver = init_webdriver()
+    if url_path.__len__() == 1:
+        q = input(
+            f"Is {url.path} path to manga that you would to download?[Y/n] ")
+        if q == "y" or q == "Y" or q == "" or q == " ":
+            print("Intializing webdriver. Please wait. This may take a while...")
+            webdriver = init_webdriver()
+            print("Webdriver successfully initialized. Loading page...")
+
+            get_manga_info(*url_path, webdriver=webdriver)
+
+        else:
+            return print("Invalid uri")
+    elif url_path.__len__() == 3:
+        q = input(
+            f"Is {url.path} path to tom and chapter that you would to download?[Y/n] ")
+        if q == "y" or q == "Y" or q == "" or q == " ":
+            print("Intializing webdriver. Please wait. This may take a while...")
+            webdriver = init_webdriver()
+            print("Webdriver successfully initialized. Downloading...")
+
+            url_path[1] = int(url_path[1].removeprefix('v'))
+            url_path[2] = int(url_path[2].removeprefix('c'))
+
+            download_chapter(*url_path, webdriver=webdriver)
+
+        else:
+            return print("Invalid uri")
+    else:
+        return print("Invalid uri")
 
     webdriver.close()
     webdriver.quit()
 
 
 def find():
+    pass
+
+
+def download_chapter(manga_name: string, tom_num: int, chapter_num: int, webdriver: webdriver.Chrome):
+    #
+
+    url = f"https://mangalib.me/{manga_name}/v{tom_num}/c{chapter_num}"
+    webdriver.get(url)
+
+    # ls = webdriver.getLocalStorage()
+    # ls.setItem('reader', '{"desableHeader":false,"caution":false,"theme":"dark","mode":"vertical","containerWidth":960,"imageServer":"compress","imageFit":"width","showComments":false,"commentsSize":500,"clickTargetArea":"image","disableHeader":false,"turningPage":"image","zoom":"disabled"}')
+
+    if urlparse(url).path != urlparse(webdriver.current_url).path:
+
+        print(url)
+        print(webdriver.current_url)
+
+        with open('page_data.html', 'w', encoding='utf-8') as f:
+            f.write(webdriver.page_source)
+
+        webdriver.close()
+        webdriver.quit()
+        raise Exception(
+            "Something went wrong. Page url does not equals webdriver url. Seems like redirect. Page data has been saved to page_data.html")
+
+    # By default mangalib loads only 2 first manga pages
+    # So we cannot simply parse it
+    # Loading one page and then accessing new one like viewers
+
+    # Getting all pages
+    pages = webdriver.find_elements(
+        By.XPATH, "//select[@id='reader-pages']/option")
+
+    if not pages:
+        with open('page_data.html', 'w', encoding='utf-8') as f:
+            f.write(webdriver.page_source)
+
+        webdriver.close()
+        webdriver.quit()
+        raise Exception(
+            "Something went wrong. Cannot find manga pages. Page data has been saved to page_data.html")
+
+    for page in pages:
+        page.click()
+
+        page_num = page.get_attribute('value')
+
+        manga_img = webdriver.find_element(
+            By.XPATH,
+            f"//div[@class='reader']/div[@class='reader-view']/div[@class='reader-view__container']/div[@data-p='{page_num}']/img"
+        ).get_attribute('src')
+
+        print(manga_img)
+
+    # content = webdriver.page_source
+
+    # soup = BeautifulSoup(content, 'html.parser')
+
+    # div_img_wraps = soup.find('div', {"class": "reader"}) \
+    #     .find('div', {"class": "reader-view"}) \
+    #     .find('div', {"class": "reader-view__container"}) \
+    #     .find_all('div', {"class": "reader-view__wrap"})
+
+    # for div_img in div_img_wraps:
+    #     img = div_img.find('img')
+    #     print(img)
+
+    # # print(soup)
+
+
+def download_tom(manga_name, tom_num, webdriver):
+    pass
+
+
+def get_manga_info(manga_name, webdriver):
     pass
