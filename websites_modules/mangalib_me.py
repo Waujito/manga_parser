b357a363d9100d72703f84e4088b0b63220f21ab
Vetrov Vadim <vetrovvd@yandex.ru>
2022-06-29 17:43:43 +0300

    All manga download

--- websites_modules/mangalib_me.py
+++ websites_modules/mangalib_me.py
@@ -53,7 +53,7 @@ def download_by_url():
 
     if url_path.__len__() == 1:
         q = input(
-            f"Is {url.path} path to manga that you would to download?[Y/n] ")
+            f"Is {url.path} path to manga that you want to download? [Y/n] ")
         if q == "y" or q == "Y" or q == "" or q == " ":
             print("Intializing webdriver. Please wait. This may take a while...")
             webdriver = init_webdriver()
@@ -65,7 +65,7 @@ def download_by_url():
             return print("Invalid uri")
     elif url_path.__len__() == 3:
         q = input(
-            f"Is {url.path} path to tom and chapter that you would to download?[Y/n] ")
+            f"Is {url.path} path to tom and chapter that you want to download? [Y/n] ")
         if q == "y" or q == "Y" or q == "" or q == " ":
             print("Intializing webdriver. Please wait. This may take a while...")
             webdriver = init_webdriver()
@@ -81,6 +81,7 @@ def download_by_url():
     else:
         return print("Invalid uri")
 
+    print("Closing session...")
     webdriver.close()
     webdriver.quit()
 
@@ -173,5 +174,109 @@ def download_tom(manga_name, tom_num, webdriver):
     pass
 
 
-def get_manga_info(manga_name, webdriver):
-    pass
+def get_manga_info(manga_name: string, webdriver: webdriver.Chrome):
+    url = f"https://mangalib.me/{manga_name}?section=chapters"
+    webdriver.get(url)
+
+    # Check driver url and manga url
+    if urlparse(url).path != urlparse(webdriver.current_url).path:
+
+        print(url)
+        print(webdriver.current_url)
+
+        with open('page_data.html', 'w', encoding='utf-8') as f:
+            f.write(webdriver.page_source)
+
+        webdriver.close()
+        webdriver.quit()
+        raise Exception(
+            "Something went wrong. Page url does not equals webdriver url. Seems like redirect. Page data has been saved to page_data.html")
+
+    print("Page successfully loaded. Parsing...")
+    print("Scrolling page from first chapter to last...")
+    print("Warning! The process may loop")
+    print('If it happenes please restart')
+    print('If this keeps please report it https://github.com/Waujito/manga_parser/issues')
+
+    # Getting all toms and chapters
+    scroll_n = 100
+    toms_chapters = []
+
+    bheight = webdriver.find_element(By.TAG_NAME, 'body').size['height']
+
+    tss = {}
+    lt = ""
+
+    chapters = {}
+
+    while True:
+        tcs = webdriver.find_elements(
+            By.CSS_SELECTOR,
+            ".tabs__content .media-chapters .media-chapters-list .vue-recycle-scroller .vue-recycle-scroller__item-wrapper .vue-recycle-scroller__item-view"
+        )
+
+        if not tcs:
+            return print("Cannot find chapters. Seems like 404 error.")
+
+        webdriver.execute_script(f'window.scrollTo(0,{scroll_n})')
+
+        scroll_n += 1000
+
+        print(f"Scrolling on {int(scroll_n/bheight*100)}%")
+
+        elh = tcs[-1].find_element(
+            By.CSS_SELECTOR,
+            '.media-chapter .media-chapter__body .media-chapter__name a'
+        ).get_attribute('href')
+
+        if lt == elh:
+            continue
+        else:
+            lt = elh
+
+        print(f"Current page {elh}")
+
+        for tc in tcs:
+
+            tc_text = tc.find_element(
+                By.CSS_SELECTOR,
+                '.media-chapter .media-chapter__body .media-chapter__name a'
+            ).text
+
+            ts = tc_text.split(' ')
+            for k in range(0, ts.__len__(), 1):
+                if ts[k] == "Том":
+                    tom = int(ts[k+1])
+
+                if ts[k] == "Глава":
+                    chapter = int(ts[k+1])
+
+            if not chapters.get(tom):
+                chapters[tom] = {chapter: chapter}
+            elif not chapters[tom].get(chapter):
+                chapters[tom][chapter] = chapter
+
+        if urlparse(elh).path.endswith('v1/c1'):
+            break
+
+    print("Information collected successfull")
+    print(chapters)
+
+    print("What do you want to do?")
+    print("[1] Download range of toms")
+    print("[2] Download range of chapters")
+    print("[3] Download ALL manga")
+    q = input()
+    match q:
+        case '1':
+            pass
+        case '2':
+            pass
+        case '3':
+            print("Downloading manga...")
+            for i in chapters:
+                for c in chapters[i]:
+                    print(f"Downloading tom {i} chapter {chapters[i][c]}")
+                    download_chapter(manga_name, i, chapters[i][c], webdriver)
+        case _:
+            return print("Incorrect number")
