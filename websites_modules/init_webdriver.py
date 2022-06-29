bb624c513aeb4a01a4f18ddddb29883b0f91ea5c
Vetrov Vadim <vetrovvd@yandex.ru>
2022-06-29 12:55:54 +0300

    webdriver with chrome from file

--- websites_modules/init_webdriver.py
+++ websites_modules/init_webdriver.py
@@ -1,11 +1,19 @@
 import undetected_chromedriver
 import time
 from selenium import webdriver
+from pathlib import Path
 
 
 def init_webdriver():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    driver = undetected_chromedriver.Chrome(options)
+    options.add_argument("--headless")  # Start chrome in silent mode
+
+    # If driver file exists run chrome from driver
+    # Else download driver
+    if Path(Path(__file__).parent.resolve(), 'chromedriver.exe').exists():
+        driver = undetected_chromedriver.Chrome(options=options, driver_executable_path=Path(
+            Path(__file__).parent.resolve(), 'chromedriver.exe').__str__())
+    else:
+        driver = undetected_chromedriver.Chrome(options=options)
 
     return driver
